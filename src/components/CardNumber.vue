d3b12c4963fb27f3bcea993e5906ef13e2563fa9
tania-abitbol <tabitbol@hotmail.com>
2020-06-18 11:58:03 +0200

    style card

--- src/components/CardNumber.vue
+++ src/components/CardNumber.vue
@@ -1,9 +1,25 @@
 <template>
-  <div></div>
+  <div class="card">
+    <p>A MOMENT OF HISTORY</p>
+    <p class="card__number">you enter 3 dates...</p>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.card {
+  text-align: center;
+  width: 15%;
+  padding: 20px;
+  background-color: black;
+  color: white;
+  font-family: Helvetica, sans-serif;
+  font-size: 18px;
+  &__number {
+    font-weight: bold;
+  }
+}
+</style>
