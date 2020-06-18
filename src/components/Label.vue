38d5bf229a9c41edae55773c9bd8c98f7ec04c0c
tania-abitbol <tabitbol@hotmail.com>
2020-06-18 11:47:49 +0200

    create trapeze and label style

--- src/components/Label.vue
+++ src/components/Label.vue
@@ -1,8 +1,16 @@
 <template>
-  <div>
-    <div class="trapeze"></div>
-    <label for="label">Please enter dates separated by commas :</label>
-    <input type="text" name="label" id="label" placeholder="17/04, 13/03, 10/04" />
+  <div class="label">
+    <div class="trapeze">
+      <div class="label__block">
+        <label for="label">Please enter dates separated by commas :</label>
+        <input
+          type="text"
+          name="label"
+          id="label"
+          placeholder="17/04, 13/03, 10/04"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,13 +18,39 @@
 export default { name: "Label" };
 </script>
 
-
 <style scoped lang="scss">
+.label {
+  border-block: none;
+  border: none;
+  font-family: Helvetica, sans-serif;
+  font-size: 18px;
+  font-weight: lighter;
+  & label {
+    padding-bottom: 10px;
+  }
+  & input {
+    text-align: center;
+    font-size: 14px;
+    font-weight: bold;
+    background: transparent;
+    border: none;
+    border-bottom: 2px solid black;
+  }
+  &__block {
+    display: flex;
+    flex-direction: column;
+  }
+}
 .trapeze {
-  border-bottom: 50px solid blue;
-  border-left: 25px solid transparent;
-  border-right: 25px solid transparent;
-  height: 0;
-  width: 50px;
+  background-color: rgb(42, 184, 231);
+  width: fit-content;
+  padding: 5% 5%;
+  padding-bottom: 80px;
+  clip-path: polygon(0 0, 100% 0, 100% 60%, 0% 100%);
+  width: fit-content;
+  height: 100px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 </style>
